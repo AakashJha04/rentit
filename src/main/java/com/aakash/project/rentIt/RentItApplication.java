fb6094db21f33a83b1238bb08958a4cd2e58b644
AakashJha04 <aakashthedeveloper04@gmail.com>
2025-05-06 17:00:11 +0530

    Initial commit for RentIt project

---
+++ src/main/java/com/aakash/project/rentIt/RentItApplication.java
@@ -0,0 +1,13 @@
+package com.aakash.project.rentIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentItApplication.class, args);
+	}
+
+}
